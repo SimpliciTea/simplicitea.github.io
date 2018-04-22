40e44d201a5d175d3d485f1d2c6f68ebee4451a5
John Ley <ley.johnt@gmail.com>
2018-04-21 18:18:04 -0700

    Mobile menu adjustments
    -- added toggle event to link-clicking to close menu when a link is clicked
    -- menu now only gains oj background when below header

--- js/bundle.js
+++ js/bundle.js
@@ -19,10 +19,12 @@ require('./rainbow.js');
    */
   Menu.prototype._init = function() {
     this.body = document.body;
+    this.wrapper = document.querySelector('.nav-mobile');
     this.menu = document.querySelector('.menu-mobile')
     this.menuItems = document.querySelectorAll('.menu-item');
     this.backdrop = document.querySelector('#menu-mobile-backdrop');
     this.toggleBtn = document.querySelector('.menu-toggle');
+    this.lastScrollPosition = 0;
 
     this._initEvents();
   }
@@ -31,9 +33,23 @@ require('./rainbow.js');
    *  Initialize menu events
    */
   Menu.prototype._initEvents = function() {
+    
+    // menu toggle handler
     this.toggleBtn.addEventListener('click', (e) => {
       e.preventDefault();
       this._toggle();
+    })
+
+    // sticky menu handler
+    window.addEventListener('scroll', (e) => {
+      this._handlePageScroll();
+    })
+
+    // toggle menu off on link click
+    this.menuItems.forEach(item => {
+      item.addEventListener('click', (e) => {
+        this._toggle();
+      })
     })
   }
 
@@ -48,6 +64,37 @@ require('./rainbow.js');
       item.classList.toggle('active');
     })
     this.toggleBtn.classList.toggle('active');
+  }
+
+  /*
+   *  Toggle Navbar Anchor
+   */
+  Menu.prototype._handlePageScroll = function() {
+    var currY = window.scrollY;
+    var header = document.querySelector('header');
+
+    // scrolling down?
+    if (currY > this.lastScrollPosition) {
+
+      // when below header, disappear
+      if (currY > header.clientHeight - this.wrapper.clientHeight) {
+        console.log('hit');
+        if (!this.wrapper.classList.contains('nav-mobile--anchored')) {
+          this.wrapper.classList.add('nav-mobile--anchored');
+        }
+      }
+    }
+
+    // must be scrolling up!
+    else {
+      if (currY < header.clientHeight - 50) {
+        if (this.wrapper.classList.contains('nav-mobile--anchored')) {
+          this.wrapper.classList.remove('nav-mobile--anchored');
+        }
+      }
+    }
+
+    this.lastScrollPosition = currY;
   }
 
   window.Menu = Menu;
